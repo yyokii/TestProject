4c62a9e5c8acbb258d2b65438f3b568083057720
yoki <yyokii.h@gmail.com>
2020-04-02 01:29:44 +0900

    demo constraint

---
+++ Test/CustomView/ArticleView.swift
@@ -0,0 +1,29 @@
+//
+//  ArticleView.swift
+//  ReadingList
+//
+//  Created by 東原与生 on 2019/08/30.
+//  Copyright © 2019 Yoki Higashihara. All rights reserved.
+//
+
+import UIKit
+
+class ArticleView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadNib()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        loadNib()
+    }
+    
+    func loadNib() {
+        if let view = Bundle(for: type(of: self)).loadNibNamed(String(describing: type(of: self)), owner: self, options: nil)?.first as? UIView {
+            view.frame = self.bounds
+            self.addSubview(view)
+        }
+    }
+}
